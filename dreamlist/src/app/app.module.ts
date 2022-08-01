35259f817aaddbbe7f562199788e8ce07fb6efcc
PU MAC2 <pumac2@PUMAC2.local>
2022-08-01 14:54:24 +0200

    push

--- dreamlist/src/app/app.module.ts
+++ dreamlist/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenusideComponent } from './components/menuside/menuside.component';
+import { CalenderComponent } from './components/calender/calender.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MenusideComponent,
+    CalenderComponent
   ],
   imports: [
     BrowserModule,
---
+++ dreamlist/src/app/components/menuside/menuside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menuside',
+  templateUrl: './menuside.component.html',
+  styleUrls: ['./menuside.component.css']
+})
+export class MenusideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
