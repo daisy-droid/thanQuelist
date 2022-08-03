136ca8062d4b00dd794ba4bd11345e4823b24b3b
PU MAC2 <pumac2@PUMAC2.local>
2022-08-03 16:09:26 +0200

    push

--- dreamlist/src/app/app.module.ts
+++ dreamlist/src/app/app.module.ts
@@ -5,18 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MenusideComponent } from './components/menuside/menuside.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CalenderComponent } from './components/calender/calender.component';
+
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ThemeComponent } from './components/theme/theme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MenusideComponent,
-    CalenderComponent
+    CalenderComponent,
+  
+    LoginComponent,
+       ThemeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    CalendarModule.forRoot({
+    provide: DateAdapter,
+    useFactory: adapterFactory,
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
