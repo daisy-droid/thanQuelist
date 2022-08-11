81cf3be223775fc885fdf0d86aacefde7f6690f9
Ipeleng <ipeleng.mothibi@dadevs.co.za>
2022-08-11 22:50:51 +0200

    get function updated

--- frontend/src/app/components/today/today.component.ts
+++ frontend/src/app/components/today/today.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormsModule, FormBuilder , ReactiveFormsModule } from '@angular/forms';
 import { Route, Router } from '@angular/router';
+import { response } from 'express';
 import { CreatecardService } from 'src/app/services/createcard.service';
 import { GetallService } from 'src/app/services/getall.service';
+import { threadId } from 'worker_threads';
 
 
 
@@ -19,11 +21,14 @@ export class TodayComponent implements OnInit {
 
  
   submitted: any;
+  card:any ;
    
    constructor(private getallservice :GetallService, private reactiveformmodule: ReactiveFormsModule, private formBuilder:FormBuilder, private router: Router) { }
  
    ngOnInit(): void {
     this.get()
+
+
    this.Form =  new FormGroup({
  
    entryId : new FormControl(''), 
@@ -33,15 +38,23 @@ export class TodayComponent implements OnInit {
  })
    }
  
-   get(){
+   /*get(){
      //console.log(this.Form.value);
      this.getallservice.getall().subscribe((respond : any) =>{
-       this.submitted=true;
+      this.card=response
        console.log(respond)
-     })
+     })*/
+     get()
+     {
+      this.getallservice.getall().subscribe(
+      data=>{
+        this.card=data
+      })
+     }
+
      
    }
 
-  card: any = [1,2,3,3,3,3]
+ // this.card=response any = [1,2,3,3,3,3]
   
-}
+
